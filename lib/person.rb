ac08adbe30a33ada0330ee02acf796ca2dd89550
jrcliff <justoon7.jc@gmail.com>
2020-10-07 17:50:07 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,18 +1,81 @@
 # your code goes here
 require 'pry'
-class Person    
 
-    def initialize(persons_name, bank_account = 25, happiness = 8)        
-        @name = persons_name        
-        @bank_account = bank_account        
-        @happiness = happiness    
-    end    
-    def name        
-        @name    
-    end    
-    attr_accessor :bank_account    
-    def happiness        
-        @happiness    
-    end        
+class Person
+    attr_accessor :bank_account
     attr_accessor :happiness
+    attr_accessor :hygiene
+    def initialize(persons_name, bank_account = 25, happiness = 8, hygiene = 8)
+        @name = persons_name
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygiene = hygiene
     end
+
+
+    def name
+        @name
+    end
+
+    def happiness
+        @happiness.clamp(0,10)
+    end
+
+
+    def hygiene
+        @hygiene.clamp(0,10)
+    end
+
+    def clean?
+        if @hygiene > 7
+            return true
+        else
+            return false
+        end
+    end
+
+    def happy?
+        if @happiness > 7
+            return true
+        else
+            return false
+        end
+    end
+
+    def get_paid(salary)
+        @bank_account += salary
+        return "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.happiness += 2
+        self.hygiene -= 3
+        return "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        return "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(person, topic)
+        case topic
+        when topic = "politics"
+            self.happiness -= 2
+            person.happiness -= 2
+            return "blah blah partisan blah lobbyist"
+        when topic = "weather"
+            self.happiness += 1
+            person.happiness += 1
+            return "blah blah sun blah rain"
+        else
+            return "blah blah blah blah blah"
+        end
+    end
+end
