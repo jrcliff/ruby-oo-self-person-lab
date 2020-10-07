73d27cc5ad4c1dfa9fd5e166d98c402e3a6ca0a4
jrcliff <justoon7.jc@gmail.com>
2020-10-07 15:35:06 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,18 @@
 # your code goes here
+require 'pry'
+class Person    
+
+    def initialize(persons_name, bank_account = 25, happiness = 8)        
+        @name = persons_name        
+        @bank_account = bank_account        
+        @happiness = happiness    
+    end    
+    def name        
+        @name    
+    end    
+    attr_accessor :bank_account    
+    def happiness        
+        @happiness    
+    end        
+    attr_accessor :happiness
+    end
